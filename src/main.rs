781a3dedd666a86eae627e209ba622b485cc0697
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2022-07-07 12:17:41 -0800

    install to cargo bin & program files

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,24 @@ use std::{
 };
 
 const DOWNLOAD_PATH: &str =
-    "https://github.com/metaplex-foundation/sugar/releases/latest/download/sugar-macos-m1-latest";
+    "https://github.com/metaplex-foundation/sugar/releases/latest/download/sugar-windows-latest.exe";
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let home = if cfg!(unix) {
-        println!("For Unix systems use the install script in the Sugar README.");
+fn main() -> Result<()> {
+    if !(cfg!(windows)) {
+        println!("For Linux and MacOS systems use the install script in the Sugar README.");
         std::process::exit(1);
-    } else if cfg!(windows) {
-        let drive = env::var_os("HOMEDRIVE").expect("Coulnd't find Windows home drive key.");
-        let path = env::var_os("HOMEPATH").expect("Coulnd't find Windows home path key.");
-        Path::new(&drive).join(&path).as_os_str().to_owned()
-    } else if cfg!(target_os = "macos") {
-        println!("For MacOS systems use the install script in the Sugar README.");
-        std::process::exit(1);
-    } else {
-        panic!("Unsupported OS!");
-    };
+    }
+
+    let drive = env::var_os("HOMEDRIVE").expect("Coulnd't find Windows home drive key.");
+    let path = env::var_os("HOMEPATH").expect("Coulnd't find Windows home path key.");
+    let home = Path::new(&drive).join(&path).as_os_str().to_owned();
 
     let cargo_bin_path = Path::new(&home).join(".cargo").join("bin");
-    let program_files_path = Path::new(&home).join("Program Files");
+    let program_files_path = Path::new(&drive).join(r"\Program Files");
 
     // Create temporary directory for downloaded binary. `mktemp` drops the file when the variable goes out of scope.
     let temp = Temp::new_file()?;
     let temp_file_path = temp.to_path_buf();
-    println!("Temp file: {:?}", temp_file_path);
 
     let mut f = OpenOptions::new()
         .read(true)
@@ -40,7 +33,7 @@ async fn main() -> Result<()> {
         .open(&temp_file_path)?;
 
     println!("Getting binary....");
-    let contents = reqwest::get(DOWNLOAD_PATH).await?.bytes().await?;
+    let contents = reqwest::blocking::get(DOWNLOAD_PATH)?.bytes()?;
     println!("Writing binary....");
     f.write_all(&contents)?;
 

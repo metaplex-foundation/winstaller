aeccb728cc83d679f876ef8aae2dbe07148d0261
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2022-07-07 16:54:09 -0800

    remove temp file; simplify

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use anyhow::Result;
-use mktemp::Temp;
 use std::{
     env,
     fs::{self, OpenOptions},
@@ -18,46 +17,57 @@ const DOWNLOAD_PATH: &str =
 
 fn main() -> Result<()> {
     if !cfg!(windows) {
-        println!("For Linux and MacOS systems use the install script in the Sugar README.");
+        eprintln!("For Linux and MacOS systems use the install script in the Sugar README.");
         std::process::exit(1);
     }
 
     let drive = env::var_os("HOMEDRIVE").expect("Coulnd't find Windows home drive key.");
     let path = env::var_os("HOMEPATH").expect("Coulnd't find Windows home path key.");
     let local_app_data = env::var_os("LOCALAPPDATA").expect("Coudln't find LOCALAPPDATA path key.");
+
     let home = Path::new(&drive).join(&path).as_os_str().to_owned();
-
     let cargo_bin_path = Path::new(&home).join(".cargo").join("bin");
-    let local_programs_path = Path::new(&local_app_data).join("SugarCLI");
+    let local_app_data_path = Path::new(&local_app_data).join("SugarCLI");
 
-    // Create temporary directory for downloaded binary. `mktemp` drops the file when the variable goes out of scope.
-    let temp = Temp::new_file()?;
-    let temp_file_path = temp.to_path_buf();
+    // Prefer to install to .cargo/bin if it exists, otherwise use LOCALAPPDATA.
+    if cargo_bin_path.exists() {
+        eprintln!("Installing to .cargo/bin...");
+        let mut f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&cargo_bin_path.join("sugar.exe"))?;
 
-    let mut f = OpenOptions::new()
-        .read(true)
-        .write(true)
-        .open(&temp_file_path)?;
+        fetch_binary(&mut f)?;
+    } else {
+        eprintln!("Installing to LocalAppData...");
 
-    println!("Getting binary....");
-    let contents = reqwest::blocking::get(DOWNLOAD_PATH)?.bytes()?;
-    println!("Writing binary....");
-    f.write_all(&contents)?;
+        // Create SugarCLI folder in LOCALAPPDATA if it doesn't already exist.
+        if !local_app_data_path.exists() {
+            fs::create_dir(&local_app_data_path)?;
+        }
 
-    // Make SugarCLI folder in LOCALAPPDATA if it doesn't already exist.
-    if !local_programs_path.exists() {
-        fs::create_dir(&local_programs_path)?;
+        let mut f = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(&local_app_data_path.join("sugar.exe"))?;
+
+        // Add to PATH if not already present.
+        let hkcu = RegKey::predef(HKEY_CURRENT_USER);
+        let (env, _) = hkcu.create_subkey("Environment")?;
+        let mut path: String = env.get_value("path")?;
+        if !path.contains(local_app_data_path.to_str().unwrap()) {
+            path.push(';');
+            path.push_str(local_app_data_path.to_str().unwrap());
+            env.set_value("path", &path)?;
+        }
+
+        fetch_binary(&mut f)?;
     }
 
-    // Add to PATH.
-    let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let (env, _) = hkcu.create_subkey("Environment")?;
-    let mut path: String = env.get_value("path")?;
-    path.push(';');
-    path.push_str(local_programs_path.to_str().unwrap());
-    env.set_value("path", &path)?;
-
     // Signal other processes to update their environments so the new path is registered.
+    eprintln!("Refreshing PATH...");
     unsafe {
         SendMessageTimeoutA(
             HWND_BROADCAST,
@@ -70,14 +80,14 @@ fn main() -> Result<()> {
         );
     }
 
-    // Prefer to install to .cargo/bin if it exists, otherwise use LOCALAPPDATA.
-    if cargo_bin_path.exists() {
-        println!("Installing to .cargo/bin...");
-        fs::copy(&temp_file_path, cargo_bin_path.join("sugar.exe"))?;
-    } else {
-        println!("Installing to Program Files...");
-        fs::copy(&temp_file_path, local_programs_path.join("sugar.exe"))?;
-    }
+    Ok(())
+}
+
+fn fetch_binary<F: Write>(f: &mut F) -> Result<()> {
+    eprintln!("Getting binary....");
+    let contents = reqwest::blocking::get(DOWNLOAD_PATH)?.bytes()?;
+    eprintln!("Writing binary....");
+    f.write_all(&contents)?;
 
     Ok(())
 }
